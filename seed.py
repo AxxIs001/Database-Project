dbbe599e2227f194c76f8726792cb410768f97e8
Chyngyz Abdikerimov <411021385@gms.ndhu.edu.tw>
2025-06-22 22:12:42 +0800

    second commit

--- seed.py
+++ seed.py
@@ -5,7 +5,13 @@ with app.app_context():
         Book(title='The Great Gatsby', author='F. Scott Fitzgerald', price=10.99, stock=20),
         Book(title='1984', author='George Orwell', price=8.99, stock=15),
         Book(title='To Kill a Mockingbird', author='Harper Lee', price=12.50, stock=30),
+        Book(title='The Great Gatsby', author='F. Scott Fitzgerald', price=10.99, stock=20),
+        Book(title='1984', author='George Orwell', price=8.99, stock=15),
+        Book(title='To Kill a Mockingbird', author='Harper Lee', price=12.50, stock=30),
+        Book(title='The Great Gatsby', author='F. Scott Fitzgerald', price=10.99, stock=20),
+        Book(title='1984', author='George Orwell', price=8.99, stock=15),
+        Book(title='To Kill a Mockingbird', author='Harper Lee', price=12.50, stock=30),
     ]
     db.session.add_all(books)
     db.session.commit()
-    print("✅ Sample books added.")
+    print("Sample books added.")
